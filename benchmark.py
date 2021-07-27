e0e8c39f83bff9f94930888a4e77f6f3bfa83761
rosulucian <rosu.lucian@gmail.com>
2021-07-27 15:55:47 +0300

    add results notebook

--- benchmark.py
+++ benchmark.py
@@ -13,7 +13,7 @@ def parseargs(args):
     parser.add_argument('-f', '--framework', choices=['pytorch', 'jit', 'onnx'], default='pytorch')
     parser.add_argument('-m', '--models', nargs='+', choices=model_choices+['all'], default=['alexnet'])
     parser.add_argument('-d', '--devices', nargs='+', choices=['cuda', 'cpu'], default=['cpu'])
-    parser.add_argument('-b', '--batch_size', action='store', type=int, default=7)
+    parser.add_argument('-b', '--batch_size', action='store', type=int, default=5)
     parser.add_argument('-s', '--size', action='store', type=int, default=224)
     parser.add_argument('-e', '--export', action='store_true', default=False)
     parser.add_argument('-v', '--verbose', action='store_true', default=False)
